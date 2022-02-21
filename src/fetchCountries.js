66d9ddaaafd29ff770d84b72409a513df2912314
TetyanaKhlizova <90279592+TetyanaKhlizova@users.noreply.github.com>
2022-02-21 23:12:13 +0200

    start1

---
+++ src/fetchCountries.js
@@ -0,0 +1,10 @@
+// fetchCountries() {
+//     fetch('https://restcountries.com/v3.1/name/{name}').then(responce => {
+//         console.log(responce.json());
+//         return responce.json;
+//     }).then(country => {
+//         console.log(country)
+//     }).catch(error => {
+//         console.log(error);
+//     })
+// }
